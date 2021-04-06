450a207e194c29be0e73f380c1ea44be8b51de40
Zino Schubert <1010115@hr.nl>
2021-04-06 22:52:44 +0200

    Added an Agenda page and some css styling

---
+++ agenda.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta name="HandheldFriendly" content="true">
+    <title>Theater Rotterdam Agenda</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header class="agenda-header">
+    <img src="includes/images/logo_tr_1.png" alt="Theater Rotterdam logo">
+    <h1>Agenda</h1>
+</header>
+<div class="agenda-items">
+    <h2>What is there to see?</h2>
+    <div class="item" id="1">
+        <img src="includes/images/astro-angels.jpg" alt="Astro Angels banner">
+        <h3>Astro Angels</h3>
+        <h4>2-05-2021</h4>
+    </div>
+    <div class="item" id="2">
+        <div class="flexbox">
+        <img src="includes/images/moulin-rouge.jpg" alt="Moulin Rouge banner">
+        <h3>Moulin Rouge!</h3>
+        <h4>4-07-2021</h4>
+    </div>
+
+</body>
+</html>
